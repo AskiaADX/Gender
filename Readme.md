c5a38304596002177c0e77b91fed02a228e1e093
jeromed <jeromed@askia.com>
2015-05-20 17:50:29 +0200

    first commit
    first version of the adc2-gender control

---
+++ Readme.md
@@ -0,0 +1,27 @@
+Gender
+=====
+
+Preview
+-------
+
+[Preview of Gender](http://show.askia.com/WebProd/cgi-bin/AskiaExt.dll?Action=StartSurvey&SurveyName=ADC2_Gender)
+        
+Tested in
+-----------
+
+    This control was successfully tested in Chrome, Firefox, Safari, Opera and IE8+.
+          
+Dependencies
+-----------------
+
+    * No dependencies (HTML, CSS, Javascript (Vanilla))
+        
+Notes
+-------
+
+    - Some parameters can have different rendering depending of the browser used.
+    - The Border Radius will not be rendered for IE8 and lower version of Internet Explorer.
+    - The Drop Shadow will not be rendered for IE9 and lower version of Internet Explorer.
+    
+![screenshot](/examples/adc2-gender.png)
+    
---
+++ resources/static/adc2-gender.js
@@ -0,0 +1,108 @@
+ (function() {
+        (function init() {
+            polyfillGetElementsByClassName();
+            var containers = document.getElementsByClassName("adc-gender");
+            for (var j = 0, l = containers.length; j < l; j++) {
+                var labels = containers[j].getElementsByTagName("label");
+                var inputs = containers[j].getElementsByTagName("input");
+                var autoFwd = (hasClass(containers[j], "autoforward")) ? true : false;
+                for (var i = 0, k = labels.length; i < k; i++) {
+                    labels[i].onclick = checkIt;
+                    inputs[i].onclick = checkClass(i,autoFwd);
+                    labels[i].onmouseover = hoverClass(i);
+                    labels[i].onmouseout = outClass(i);
+                }
+            }
+        }());
+
+        // Polyfill: Add a getElementsByClassName function IE < 9
+        function polyfillGetElementsByClassName() {
+            if (!document.getElementsByClassName) {
+                document.getElementsByClassName = function(search) {
+                    var d = document, elements, pattern, i, results = [];
+                    if (d.querySelectorAll) { // IE8
+                        return d.querySelectorAll("." + search);
+                    }
+                    if (d.evaluate) { // IE6, IE7
+                        pattern = ".//*[contains(concat(' ', @class, ' '), ' " + search + " ')]";
+                        elements = d.evaluate(pattern, d, null, 0, null);
+                        while ((i = elements.iterateNext())) {
+                            results.push(i);
+                        }
+                    } else {
+                        elements = d.getElementsByTagName("*");
+                        pattern = new RegExp("(^|\\s)" + search + "(\\s|$)");
+                        for (var j = 0, l = elements.length; j < l; j++) {
+                            if ( pattern.test(elements[j].className) ) {
+                                results.push(elements[j]);
+                            }
+                        }
+                    }
+                    return results;
+                }
+            }
+        }
+
+        function checkIt() {
+            var input = document.getElementById(this.htmlFor);
+            input.click();
+        }
+
+        function hasClass(ele, cls) {
+            return ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+        }
+
+        function addClass(ele, cls) {
+            if (!hasClass(ele, cls)) ele.className += " " + cls;
+        }
+
+        function removeClass(ele, cls) {
+            if (hasClass(ele, cls)) {
+                var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+                ele.className = ele.className.replace(reg, '');
+            }
+        }
+
+        function checkClass(index, bool) {
+            return function () {
+                var cls = (index == 0) ? "childOne" : "childTwo";
+                var clsSel = (index == 0) ? "childOneSelected" : "childTwoSelected";
+                var clsOther = (index == 0) ? "childTwo" : "childOne";
+                var clsOtherSel = (index == 0) ? "childTwoSelected" : "childOneSelected";
+                var container = this.parentNode.parentNode;
+                var obj = container.getElementsByTagName("img")[index];
+                var objOther = container.getElementsByTagName("img")[(index == 0) ? 1 : 0];
+                removeClass(objOther, clsOtherSel);
+                addClass(objOther, clsOther);
+                removeClass(obj, cls);
+                addClass(obj, clsSel);
+                if (bool) document.getElementsByName("Next")[(document.getElementsByName("Next").length - 1)].click();
+            }
+        }
+
+        function hoverClass(index) {
+            return function () {
+                var input = document.getElementById(this.htmlFor);
+                var cls = (index == 0) ? "childOne" : "childTwo";
+                var clsHover = (index == 0) ? "childOneHover" : "childTwoHover";
+                var obj = this.children[0].children[0];
+                removeClass(obj, cls);
+                if (!input.checked) {
+                    addClass(obj, clsHover);
+                }
+            }
+        }
+
+        function outClass(index) {
+            return function () {
+                var input = document.getElementById(this.htmlFor);
+                var cls = (index == 0) ? "childOne" : "childTwo";
+                var clsHover = (index == 0) ? "childOneHover" : "childTwoHover";
+                var obj = this.children[0].children[0];
+                removeClass(obj, clsHover);
+                if (!input.checked) {
+                    addClass(obj, cls);
+                }
+            }
+        }
+ }());
